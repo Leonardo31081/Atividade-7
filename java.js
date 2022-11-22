e50bf463e44573df12ffb2b95e8598a7bb5a4b25
Leonardo Machado <85460870+Leonardo31081@users.noreply.github.com>
2022-11-21 21:24:03 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const nome = prompt ('digite seu nome')
+const idade = prompt (' diite seu ano de nascimento')
+let x;
+x = 2022 - idade;
+
+document.write(" Olá,seu nome é " + nome + " e você tem " + x + "anos");
